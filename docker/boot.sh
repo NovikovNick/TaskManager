8e3d4931768452f34e1ca417e13805bc70196af0
nnovikov <nicknovikov10@gmail.com>
2020-01-25 16:35:18 +0300

    remove redundant dockerfiles

--- docker/boot.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /srv/app
-java -jar ./app.jar
--- docker/db/postgres/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER metalheart WITH password 'metalheart';
-    CREATE DATABASE taskmanager;
-    GRANT ALL PRIVILEGES ON DATABASE taskmanager TO metalheart;
-EOSQL
